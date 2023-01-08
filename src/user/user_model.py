651a8d45b9cfeff5caf4795d0f9a3c4a1c56c148
Tanielle Arthur <taniellecurtis@Tanielles-MacBook-Pro.local>
2023-01-08 16:37:46 -0500

    add tablename to model

--- src/user/user_model.py
+++ src/user/user_model.py
@@ -5,6 +5,8 @@ import uuid
 
 
 class User(Base):
+    __tablename__ = "users"
+
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
