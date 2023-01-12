337f1091b6735631f1120205c2d4738d0707f2b3
Quincy Arthur <quincya@nib-bahamas.com>
2023-01-12 17:51:39 -0500

    add CORS

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from db.config import engine, Base
 from src.user import user_controller
+from fastapi.middleware.cors import CORSMiddleware
+
+ALLOWED_HOSTS = ["*"]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_HOSTS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user_controller.router)
 
