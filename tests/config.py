bc8c205a88d3ecde3ed8d5ebe50d16f822d78599
Tanielle Arthur <taniellecurtis@Tanielles-MacBook-Pro.local>
2023-02-12 01:26:50 -0500

    remove comment and white space

--- tests/config.py
+++ tests/config.py
@@ -1,10 +1,9 @@
 import pytest
 from starlette.testclient import TestClient
-
 from main import app
 
 
 @pytest.fixture(scope="module")
 def test_app():
     client = TestClient(app)
-    yield client  # testing happens here
+    yield client
