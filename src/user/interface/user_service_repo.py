94a898e1bba1f4a18fd191e5729b7b59d2cadcf5
Tanielle Arthur <taniellecurtis@Tanielles-MacBook-Pro.local>
2023-02-12 00:40:04 -0500

    add user repo interface

---
+++ src/user/interface/user_service_repo.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractclassmethod
+from src.user.dto.user_dto import UserDTO
+from src.user.dto.create_user_dto import CreateUserDTO
+from src.user.user_model import User
+
+
+class IUserRepo(ABC):
+    @abstractclassmethod
+    async def find_by_id(self, id: str) -> UserDTO:
+        pass
+
+    @abstractclassmethod
+    async def find_by_email(self, email: str) -> UserDTO:
+        pass
+
+    @abstractclassmethod
+    async def add_user(self, create_user: CreateUserDTO) -> UserDTO:
+        pass
+
+    @abstractclassmethod
+    def to_user_dto(self, user: User) -> UserDTO:
+        pass
+
+    @abstractclassmethod
+    async def update_password(self, user: UserDTO) -> None:
+        pass
+
+    @abstractclassmethod
+    async def update_activation_flag(self, user: UserDTO, activated: bool) -> None:
+        pass
