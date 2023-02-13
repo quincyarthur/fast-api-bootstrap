cbaf4f62f80272b33cdf5a23ae1c3aeef6e86445
Quincy Arthur <quincya@nib-bahamas.com>
2023-02-13 18:33:18 -0500

    centralized anyio usiing fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,11 @@ from tests.user.mock_user_repo import MockUserRepo
 from src.user.user_service import UserService
 
 
+@pytest.fixture
+def anyio_backend():
+    return "asyncio"
+
+
 @pytest.fixture()
 def setup_user_service() -> UserService:
     user_service = UserService(user_repo=MockUserRepo())
--- tests/user/test_user_service.py
+++ tests/user/test_user_service.py
@@ -7,39 +7,38 @@ from src.user.dto.create_user_dto import CreateUserDTO
 from src.user.enum.user_origins import UserOrigins
 
 
-@pytest.mark.anyio
-async def test_find_by_id_returns_UserDTO_if_found(setup_user_service: UserService):
+async def test_find_by_id_returns_UserDTO_if_found(
+    setup_user_service: UserService, anyio_backend
+):
     user = await setup_user_service.find_by_id(id=1)
     assert type(user) is UserDTO
 
 
-@pytest.mark.anyio
 async def test_find_by_id_returns_exception_if_not_found(
-    setup_user_service: UserService,
+    setup_user_service: UserService, anyio_backend
 ):
     with pytest.raises(HTTPException) as exc:
         await setup_user_service.find_by_id(id=2)
         assert str(exc.value) == UserExceptions.ID_NOT_FOUND.value
 
 
-@pytest.mark.anyio
-async def test_find_by_email_returns_UserDTO_if_found(setup_user_service: UserService):
+async def test_find_by_email_returns_UserDTO_if_found(
+    setup_user_service: UserService, anyio_backend
+):
     user = await setup_user_service.find_by_email(email="johndoe@gmail.com")
     assert type(user) is UserDTO
 
 
-@pytest.mark.anyio
 async def test_find_by_email_returns_exception_if_not_found(
-    setup_user_service: UserService,
+    setup_user_service: UserService, anyio_backend
 ):
     with pytest.raises(HTTPException) as exc:
         await setup_user_service.find_by_email(email="unknown@gmail.com")
         assert str(exc.value) == UserExceptions.EMAIL_NOT_FOUND.value
 
 
-@pytest.mark.anyio
 async def test_add_user_returns_exception_if_email_exists(
-    setup_user_service: UserService,
+    setup_user_service: UserService, anyio_backend
 ):
     create_user = CreateUserDTO(
         first_name="John",
@@ -53,8 +52,9 @@ async def test_add_user_returns_exception_if_email_exists(
         assert str(exc.value) == UserExceptions.EMAIL_EXISTS.value
 
 
-@pytest.mark.anyio
-async def test_exclude_password_returns_null_password(setup_user_service: UserService):
+async def test_exclude_password_returns_null_password(
+    setup_user_service: UserService, anyio_backend
+):
     user = UserDTO(
         first_name="John",
         last_name="Doe",
@@ -69,18 +69,16 @@ async def test_exclude_password_returns_null_password(setup_user_service: UserSe
     assert user.password is None
 
 
-@pytest.mark.anyio
 async def test_validate_email_returns_str_if_valid_email(
-    setup_user_service: UserService,
+    setup_user_service: UserService, anyio_backend
 ):
     email = "johndoe@gmail.com"
     email = setup_user_service.valiate_email(email=email, check_deliverability=True)
     assert type(email) is str
 
 
-@pytest.mark.anyio
 async def test_validate_email_throws_exception_if_not_valid_email(
-    setup_user_service: UserService,
+    setup_user_service: UserService, anyio_backend
 ):
     email = "johndoe@dfdsffafsafd.moc"
     with pytest.raises(HTTPException) as exc:
