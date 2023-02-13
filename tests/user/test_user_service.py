98345a476711477bca199cb5ffd9bb2142511283
Quincy Arthur <quincya@nib-bahamas.com>
2023-02-13 18:05:54 -0500

    give fixture a more specific name

--- tests/user/test_user_service.py
+++ tests/user/test_user_service.py
@@ -9,39 +9,45 @@ from src.user.enum.user_origins import UserOrigins
 
 
 @pytest.fixture()
-def setup() -> UserService:
+def setup_user_service_user_service() -> UserService:
     user_service = UserService(user_repo=MockUserRepo())
     yield user_service
 
 
 @pytest.mark.anyio
-async def test_find_by_id_returns_UserDTO_if_found(setup: UserService):
-    user = await setup.find_by_id(id=1)
+async def test_find_by_id_returns_UserDTO_if_found(setup_user_service: UserService):
+    user = await setup_user_service.find_by_id(id=1)
     assert type(user) is UserDTO
 
 
 @pytest.mark.anyio
-async def test_find_by_id_returns_exception_if_not_found(setup: UserService):
+async def test_find_by_id_returns_exception_if_not_found(
+    setup_user_service: UserService,
+):
     with pytest.raises(HTTPException) as exc:
-        await setup.find_by_id(id=2)
+        await setup_user_service.find_by_id(id=2)
         assert str(exc.value) == UserExceptions.ID_NOT_FOUND.value
 
 
 @pytest.mark.anyio
-async def test_find_by_email_returns_UserDTO_if_found(setup: UserService):
-    user = await setup.find_by_email(email="johndoe@gmail.com")
+async def test_find_by_email_returns_UserDTO_if_found(setup_user_service: UserService):
+    user = await setup_user_service.find_by_email(email="johndoe@gmail.com")
     assert type(user) is UserDTO
 
 
 @pytest.mark.anyio
-async def test_find_by_email_returns_exception_if_not_found(setup: UserService):
+async def test_find_by_email_returns_exception_if_not_found(
+    setup_user_service: UserService,
+):
     with pytest.raises(HTTPException) as exc:
-        await setup.find_by_email(email="unknown@gmail.com")
+        await setup_user_service.find_by_email(email="unknown@gmail.com")
         assert str(exc.value) == UserExceptions.EMAIL_NOT_FOUND.value
 
 
 @pytest.mark.anyio
-async def test_add_user_returns_exception_if_email_exists(setup: UserService):
+async def test_add_user_returns_exception_if_email_exists(
+    setup_user_service: UserService,
+):
     create_user = CreateUserDTO(
         first_name="John",
         last_name="Doe",
@@ -50,12 +56,12 @@ async def test_add_user_returns_exception_if_email_exists(setup: UserService):
         origin=UserOrigins.LOCAL.value,
     )
     with pytest.raises(HTTPException) as exc:
-        await setup.add_user(create_user=create_user)
+        await setup_user_service.add_user(create_user=create_user)
         assert str(exc.value) == UserExceptions.EMAIL_EXISTS.value
 
 
 @pytest.mark.anyio
-async def test_exclude_password_returns_null_password(setup: UserService):
+async def test_exclude_password_returns_null_password(setup_user_service: UserService):
     user = UserDTO(
         first_name="John",
         last_name="Doe",
@@ -66,19 +72,25 @@ async def test_exclude_password_returns_null_password(setup: UserService):
         id=1,
     )
     assert user.password is not None
-    user = await setup.exclude_password(user=user)
+    user = await setup_user_service.exclude_password(user=user)
     assert user.password is None
 
 
 @pytest.mark.anyio
-async def test_validate_email_returns_str_if_valid_email(setup: UserService):
+async def test_validate_email_returns_str_if_valid_email(
+    setup_user_service: UserService,
+):
     email = "johndoe@gmail.com"
-    email = setup.valiate_email(email=email, check_deliverability=True)
+    email = setup_user_service.valiate_email(email=email, check_deliverability=True)
     assert type(email) is str
 
 
 @pytest.mark.anyio
-async def test_validate_email_throws_exception_if_not_valid_email(setup: UserService):
+async def test_validate_email_throws_exception_if_not_valid_email(
+    setup_user_service: UserService,
+):
     email = "johndoe@dfdsffafsafd.moc"
     with pytest.raises(HTTPException) as exc:
-        validated_email = setup.valiate_email(email=email, check_deliverability=True)
+        validated_email = setup_user_service.valiate_email(
+            email=email, check_deliverability=True
+        )
