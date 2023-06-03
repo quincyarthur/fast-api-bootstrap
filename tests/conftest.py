b1187ed98e675c53933a355b4fcbf1570b512c2a
quincyarthur <quincyarthur@gmail.com>
2023-06-03 02:43:59 +0000

    format conftest.py with black

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,25 +11,24 @@ from tests.email.mock_email_service import MockEmailService
 from typing import Any
 from typing import Generator
 
+
 @pytest.fixture(scope="session")
 def event_loop(request) -> Generator:
-   loop = asyncio.get_event_loop_policy().new_event_loop()
-   yield loop
-   loop.close()
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
 
 @pytest_asyncio.fixture(scope="function")
 async def async_client():
-   app.dependency_overrides[SendInBlue] = MockEmailService
-   async with AsyncClient(
-           app=app,
-           base_url=f"http://localhost:3000"
-   ) as client:
-    """
+    app.dependency_overrides[SendInBlue] = MockEmailService
+    async with AsyncClient(app=app, base_url=f"http://localhost:3000") as client:
+        """
         Create a fresh database on each test case.
-    """
-    Base.metadata.create_all(engine) 
-    yield client
-    Base.metadata.drop_all(engine)
+        """
+        Base.metadata.create_all(engine)
+        yield client
+        Base.metadata.drop_all(engine)
 
 
 @pytest.fixture()
