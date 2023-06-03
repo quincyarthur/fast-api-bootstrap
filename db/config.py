a5b74b9709380e4b99f5acf5f74d69ee530e4900
quincyarthur <quincyarthur@gmail.com>
2023-06-03 00:34:45 +0000

    use test db if using pytest session

--- db/config.py
+++ db/config.py
@@ -2,10 +2,13 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 from dotenv import load_dotenv
+from sys import modules
 
 load_dotenv()
 
-DATABASE_URL = f"postgresql+asyncpg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+db_name = os.getenv('POSTGRES_TEST_DB') if "pytest" in modules else os.getenv('POSTGRES_DB')
+
+DATABASE_URL = f"postgresql+asyncpg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{db_name}"
 
 engine = create_async_engine(DATABASE_URL, future=True, echo=True)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
