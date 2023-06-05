00cea57d38364591f9bea061cbd9d062bf46451f
quincyarthur <quincyarthur@gmail.com>
2023-06-05 17:32:53 +0000

    correct signin url

--- tests/auth/conftest.py
+++ tests/auth/conftest.py
@@ -6,5 +6,5 @@ from src.user.dto.user_dto import UserDTO
 @pytest_asyncio.fixture(scope="function")
 async def jwt(async_client, activated_user: UserDTO) -> str:
     data = {"username": activated_user.email, "password": activated_user.password}
-    response = await async_client.post("/signin/", data=data)
+    response = await async_client.post("/auth/signin/", data=data)
     return response.json().get("access_token")
