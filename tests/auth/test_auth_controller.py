18dc35d950eb5a5c475c1194d47e5d9acc4b61ae
quincyarthur <quincyarthur@gmail.com>
2023-06-13 15:07:58 +0000

    use get method to access dict key

--- tests/auth/test_auth_controller.py
+++ tests/auth/test_auth_controller.py
@@ -7,5 +7,5 @@ async def test_signin(async_client, activated_user):
     data = {"username": activated_user.email, "password": activated_user.password}
     response = await async_client.post("/auth/signin/", data=data)
     assert response.status_code == 200
-    assert type(response.json()["access_token"]) is str
+    assert type(response.json().get("access_token")) is str
     assert response.json().get("token_type") == "bearer"
