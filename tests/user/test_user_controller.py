b7aaacf369b160b8fc32ee47a627cd6f8db6fd44
quincyarthur <quincyarthur@gmail.com>
2023-08-01 06:42:48 +0000

    use dict get func vs []

--- tests/user/test_user_controller.py
+++ tests/user/test_user_controller.py
@@ -6,13 +6,13 @@ from src.user.enum.user_origins import UserOrigins
 async def test_create_user(async_client, user):
     response = await async_client.post("/user/", json=user.__dict__)
     assert response.status_code == 200
-    assert response.json()["first_name"] == user.first_name
-    assert response.json()["last_name"] == user.last_name
-    assert response.json()["email"] == user.email
-    assert response.json()["origin"] == UserOrigins.LOCAL.value
-    assert response.json()["activated"] == False
-    assert response.json()["password"] is None
-    assert response.json()["id"] is not None
+    assert response.json().get("first_name") == user.first_name
+    assert response.json().get("last_name") == user.last_name
+    assert response.json().get("email") == user.email
+    assert response.json().get("origin") == UserOrigins.LOCAL.value
+    assert response.json().get("activated") == False
+    assert response.json().get("password") is None
+    assert response.json().get("id") is not None
 
 
 # @pytest.mark.asyncio
