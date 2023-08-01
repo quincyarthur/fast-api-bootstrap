6d535a57b14a0fc81b986936d90e83d163e3b846
quincyarthur <quincyarthur@gmail.com>
2023-08-01 06:47:46 +0000

    move test_me to test_user_controller

--- tests/user/test_user_controller.py
+++ tests/user/test_user_controller.py
@@ -1,5 +1,8 @@
 import pytest
 from src.user.enum.user_origins import UserOrigins
+from typing import Any, Generator
+from httpx import AsyncClient
+from src.user.dto.create_user_dto import CreateUserDTO
 
 
 @pytest.mark.asyncio
@@ -13,6 +16,23 @@ async def test_create_user(async_client, user):
     assert response.json().get("activated") == False
     assert response.json().get("password") is None
     assert response.json().get("id") is not None
+
+
+@pytest.mark.asyncio
+async def test_me(
+    async_client: Generator[AsyncClient, Any, Any], jwt: str, user: CreateUserDTO
+):
+    response = await async_client.get(
+        "/user/me",
+        headers={"Authorization": f"Bearer {jwt}"},
+    )
+    assert response.status_code == 200
+    assert response.json().get("id") is not None
+    assert response.json().get("first_name") == user.first_name
+    assert response.json().get("last_name") == user.last_name
+    assert response.json().get("email") == user.email
+    assert response.json().get("activated") == True
+    assert response.json().get("password") == None
 
 
 # @pytest.mark.asyncio
