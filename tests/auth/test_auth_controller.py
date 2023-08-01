ccfe2dfd32d77961fba820a91f8eddb526ff8429
quincyarthur <quincyarthur@gmail.com>
2023-08-01 06:48:32 +0000

    remove test_me from auth controller

--- tests/auth/test_auth_controller.py
+++ tests/auth/test_auth_controller.py
@@ -20,20 +20,3 @@ async def test_signin(
     assert response.status_code == 200
     assert type(response.json().get("access_token")) is str
     assert response.json().get("token_type") == "bearer"
-
-
-@pytest.mark.asyncio
-async def test_me(
-    async_client: Generator[AsyncClient, Any, Any], jwt: str, user: CreateUserDTO
-):
-    response = await async_client.get(
-        "/user/me",
-        headers={"Authorization": f"Bearer {jwt}"},
-    )
-    assert response.status_code == 200
-    assert response.json().get("id") is not None
-    assert response.json().get("first_name") == user.first_name
-    assert response.json().get("last_name") == user.last_name
-    assert response.json().get("email") == user.email
-    assert response.json().get("activated") == True
-    assert response.json().get("password") == None
