7aeb3c9d425ac44ea08498a6490df19ddabf4bb2
quincyarthur <quincyarthur@gmail.com>
2023-08-10 06:25:03 +0000

    add test to ensure email validator exceptions returned

--- tests/user/test_user_controller.py
+++ tests/user/test_user_controller.py
@@ -3,6 +3,8 @@ from src.user.enum.user_origins import UserOrigins
 from typing import Any, Generator
 from httpx import AsyncClient
 from src.user.dto.create_user_dto import CreateUserDTO
+from src.user.dto.user_dto import UserDTO
+from src.user.enum.user_exceptions import UserExceptions
 
 
 @pytest.mark.asyncio
@@ -16,6 +18,35 @@ async def test_create_user(async_client, user):
     assert response.json().get("activated") == False
     assert response.json().get("password") is None
     assert response.json().get("id") is not None
+
+
+@pytest.mark.asyncio
+async def test_create_user_throws_email_exists_exceptions(
+    async_client, add_user: UserDTO, user: CreateUserDTO
+):
+    response = await async_client.post("/user/", json=user.__dict__)
+    assert response.status_code == 400
+    assert response.json().get("detail") == UserExceptions.EMAIL_EXISTS.value
+
+
+@pytest.mark.asyncio
+async def test_create_user_throws_invalid_email_syntax_exception(
+    async_client, add_user: UserDTO, user: CreateUserDTO
+):
+    user.email = "wrong_email"
+    response = await async_client.post("/user/", json=user.__dict__)
+    assert response.status_code == 400
+    assert ("one @-sign") in response.json().get("detail")
+
+
+@pytest.mark.asyncio
+async def test_create_user_throws_undeliverable_email_exception(
+    async_client, add_user: UserDTO, user: CreateUserDTO
+):
+    user.email = "wrong_email@tttttesttt.com"
+    response = await async_client.post("/user/", json=user.__dict__)
+    assert response.status_code == 400
+    assert ("does not exist") in response.json().get("detail")
 
 
 @pytest.mark.asyncio
