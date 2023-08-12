b7ad90e7e19278772947ae897c84bb65b0ee5d69
quincyarthur <quincyarthur@gmail.com>
2023-08-12 16:06:02 +0000

    add tests to make sure wrong password exception returned

--- tests/auth/test_auth_service.py
+++ tests/auth/test_auth_service.py
@@ -6,6 +6,7 @@ import utils.jwt
 from src.auth.auth_service import AuthService
 from src.auth.enum.auth_exceptions import AuthExceptions
 from src.user.user_service import UserService
+from utils.password import Password
 
 
 async def test_get_current_user_returns_token_expired_exception_if_no_token_expiration(
@@ -43,3 +44,24 @@ async def test_signin_returns_not_activated_exception_if_user_not_activated(
             email=test_user.email, password=test_user.password
         )
         assert str(exc.value) == AuthExceptions.NOT_ACTIVATED.value
+
+
+async def test_signin_returns_wrong_password_exception(
+    monkeypatch: Generator[pytest.MonkeyPatch, Any, Any],
+    anyio_backend,
+    setup_user_service: UserService,
+    test_user: UserDTO,
+):
+    pwd = Password()
+
+    async def mock_return(*args, **kargs):
+        test_user.activated = True
+        test_user.password = pwd.hash("password")
+        return test_user
+
+    with pytest.raises(HTTPException) as exc:
+        monkeypatch.setattr(setup_user_service, "find_by_email", mock_return)
+        await AuthService(user_service=setup_user_service, pwd=pwd).signin(
+            email=test_user.email, password="wrong_password"
+        )
+    assert str(exc.value.detail) == AuthExceptions.WRONG_PASSWORD.value
