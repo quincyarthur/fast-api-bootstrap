005e6c5e746c62d7ff2ee7b2d7fda4b8cf381835
quincyarthur <quincyarthur@gmail.com>
2023-08-15 05:10:48 +0000

    add expired user accounts to queue

--- src/background_jobs/add_non_activated_accounts.py
+++ src/background_jobs/add_non_activated_accounts.py
@@ -1,3 +1,16 @@
-from src.user.user_repo import UserRepo,IUserRepo
+from src.user.user_repo import UserRepo, IUserRepo
+from datetime import datetime, timedelta
+from pika import BlockingConnection, ConnectionParameters
+import json
+
 
 async def add_non_activated_accounts(user_repo: IUserRepo):
+    expiration = datetime.utcnow() - timedelta(hours=24)
+    expired_users = user_repo.find_non_activated_accounts(expiration=expiration)
+    connection = BlockingConnection(ConnectionParameters("localhost"))
+    channel = connection.channel()
+    channel.queue_declare(queue="non-activated")
+    channel.basic_publish(
+        exchange="", routing_key="non-activated", body=json.dumps(expired_users)
+    )
+    connection.close()
