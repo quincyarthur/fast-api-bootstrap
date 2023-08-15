b40474134e62e88a4b4b32e7fc5120d08d67527d
quincyarthur <quincyarthur@gmail.com>
2023-08-15 06:37:05 +0000

    handle call of async function in sync code

--- src/background_jobs/remove_expired_user_accounts.py
+++ src/background_jobs/remove_expired_user_accounts.py
@@ -1,6 +1,7 @@
 from src.user.user_repo import UserRepo, IUserRepo
 from datetime import datetime, timedelta
+import asyncio
 
 user_repo: UserRepo = UserRepo()
 expiration = datetime.utcnow() - timedelta(hours=24)
-user_repo.remove_expired_user_accounts(expiration=expiration)
+asyncio.create_task(user_repo.remove_expired_user_accounts(expiration=expiration))
