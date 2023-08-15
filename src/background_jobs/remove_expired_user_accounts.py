7a2738a47247a58c9699d6eb4f6dec25e9dfbd85
quincyarthur <quincyarthur@gmail.com>
2023-08-15 06:31:56 +0000

    remove function so code can be executed when file called

--- src/background_jobs/remove_expired_user_accounts.py
+++ src/background_jobs/remove_expired_user_accounts.py
@@ -1,7 +1,6 @@
 from src.user.user_repo import UserRepo, IUserRepo
 from datetime import datetime, timedelta
 
-
-async def remove_expired_user_accounts(user_repo: IUserRepo = UserRepo()):
-    expiration = datetime.utcnow() - timedelta(hours=24)
-    return await user_repo.remove_expired_user_accounts(expiration=expiration)
+user_repo: UserRepo = UserRepo()
+expiration = datetime.utcnow() - timedelta(hours=24)
+user_repo.remove_expired_user_accounts(expiration=expiration)
