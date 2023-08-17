42c309dd36397e3c6eb9595e43953016b8b3477b
QArthur-NIB <qarthur@nib-bahamas.com>
2023-08-17 22:52:29 +0000

    finalize job to delete expired user accounts

--- src/background_jobs/remove_expired_user_accounts.py
+++ src/background_jobs/remove_expired_user_accounts.py
@@ -1,8 +1,15 @@
+import sys
+sys.path.append('/app')
+
+from db.config import async_session
 from src.user.user_repo import UserRepo, IUserRepo
 from datetime import datetime, timedelta
 import asyncio
 
-user_repo: UserRepo = UserRepo()
-# expiration = datetime.utcnow() - timedelta(hours=24)
-expiration = datetime.utcnow() - timedelta(minutes=5)
-asyncio.create_task(user_repo.remove_expired_user_accounts(expiration=expiration))
+if __name__ == '__main__':
+    async def async_main():
+        async with async_session() as session:
+            user_repo: IUserRepo = UserRepo(db=session)
+            expiration = datetime.utcnow() - timedelta(hours=24)
+            await user_repo.remove_expired_user_accounts(expiration=expiration)
+    asyncio.run(async_main())
