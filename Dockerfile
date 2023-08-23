8db02bfc5bb6f28091d3a9eff5419e108998abe1
quincyarthur <quincyarthur@gmail.com>
2023-08-23 01:54:08 +0000

    transition from python-crontab to supervisor

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,9 @@ COPY requirements.txt requirements.txt
 #
 RUN apt-get update
 RUN apt-get install -y cron
+RUN apt-get install sudo
+RUN adduser root sudo
 RUN pip install --no-cache-dir --upgrade -r requirements.txt
+RUN sudo mkdir /var/log/remove_expired_user_accounts
+RUN sudo chown -R root:root /var/log/remove_expired_user_accounts
+RUN sudo supervisord -c /app/src/background_jobs/supervisord.conf
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from src.user import user_controller
 from src.auth import auth_controller
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
-from crontab import CronTab
 
 ALLOWED_HOSTS = ["*"]
 
@@ -21,16 +20,6 @@ app.add_middleware(
 
 app.include_router(user_controller.router)
 app.include_router(auth_controller.router)
-
-
-with CronTab(user="root") as my_cron:
-    if not list(my_cron.find_comment(comment="job_remove_expired_user_accounts")):
-        job_remove_expired_user_accounts = my_cron.new(
-            command="/app/src/background_jobs/set_env.sh /usr/local/bin/python /app/src/background_jobs/remove_expired_user_accounts.py",
-            comment="job_remove_expired_user_accounts",
-        ).minute.every(1)
-    my_cron.write()
-
 
 @app.get("/")
 async def root():
