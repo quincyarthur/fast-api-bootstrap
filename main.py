9b51d2c77631e9e229565c5aff3921bbc7b668fc
quincyarthur <quincyarthur@gmail.com>
2023-08-25 01:51:37 +0000

    add apscheduler

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from src.user import user_controller
 from src.auth import auth_controller
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
-
+import asyncio
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from src.background_jobs import remove_expired_user_accounts
 ALLOWED_HOSTS = ["*"]
 
 app = FastAPI()
@@ -20,6 +22,20 @@ app.add_middleware(
 
 app.include_router(user_controller.router)
 app.include_router(auth_controller.router)
+
+
+scheduler = AsyncIOScheduler()
+
+#start_date='2023-06-21 10:00:00'
+scheduler.add_job(remove_expired_user_accounts, 'interval', seconds=10)
+scheduler.start()
+
+try:
+    asyncio.get_event_loop().run_forever()
+except KeyboardInterrupt:
+    pass
+finally:
+    scheduler.shutdown()
 
 @app.get("/")
 async def root():
