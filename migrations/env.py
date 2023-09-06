ac1aaf2224ccbf95353b92bdeaf56a1930adf045
quincyarthur <quincyarthur@gmail.com>
2023-09-06 03:32:23 +0000

    use base metadata as alembic target

--- migrations/env.py
+++ migrations/env.py
@@ -25,7 +25,7 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 
 
-target_metadata = [User.metadata]
+target_metadata = [Base.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
